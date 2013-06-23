44754f79d40639683297ee8529ad6137e8f75eab
Ed Paradis <ed.paradis@gmail.com>
2013-06-23 11:18:32 -0700

    display digits from a byte

--- sevenSeg8.ino
+++ sevenSeg8.ino
@@ -48,11 +48,41 @@ void displayAllSegments( byte data[] )
   }  
 }
 
+/*
+MSB is A
+LSB is decimal
+
+    a
+    _  
+ f | | b
+    - g
+ e | | c
+    _   .h
+    d
+
+ abcd efgh
+*/
+byte byteToSegDigit( byte num )
+{
+  num = num % 10;  // clamp that shiiiizzz
+  //                   0            1           2            3           4          5           6            7         8           9
+  byte lookup[] = { 0b11111100, 0b01100000, 0b11011010, 0b11110010, 0b01100110, 0b10110110, 0b10111110, 0b11100000, 0b11111110, 0b11100110 };
+  return lookup[num];
+}
+  
+    
 
 byte count = 1;
-byte digit = 1;
 byte displayData[8] = { 0xFF, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+int lastTime = 0;
 void loop()
 {
-  displayAllSegments(displayData);  // call this as fast as possible to avoid obvious scanning  
+  displayData[0] = byteToSegDigit( count);
+  displayAllSegments(displayData);  // call this as fast as possible to avoid obvious scanning
+  
+  if( millis() > lastTime + 1000)
+  { 
+    count += 1;
+    lastTime = millis();
+  }
 }
