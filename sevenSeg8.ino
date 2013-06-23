c0175d3fda950eb0adcf2cc6ea44e5e3458700eb
Ed Paradis <ed.paradis@gmail.com>
2013-06-23 10:25:15 -0700

    setup the shift register pins

---
+++ sevenSeg8.ino
@@ -0,0 +1,19 @@
+
+int dataPin = 9;
+int clockPin = 8;
+int rclockPin = 7;
+
+void setup()
+{
+  pinMode( dataPin, OUTPUT);
+  pinMode( clockPin, OUTPUT);
+  pinMode( rclockPin, OUTPUT);
+
+}
+
+
+
+void loop()
+{
+  
+}
