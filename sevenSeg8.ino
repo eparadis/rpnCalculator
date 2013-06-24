092f0deee0cf1fbbaf814edbea2bad3d07de2ce1
Ed Paradis <ed.paradis@gmail.com>
2013-06-23 23:34:35 -0700

    display a float with sign and fixed number of digits and position

--- sevenSeg8.ino
+++ sevenSeg8.ino
@@ -3,16 +3,7 @@ int dataPin = 9;
 int clockPin = 8;
 int rclockPin = 7;
 
-void setup()
-{
-  pinMode( dataPin, OUTPUT);
-  pinMode( clockPin, OUTPUT);
-  pinMode( rclockPin, OUTPUT);
 
-  digitalWrite( rclockPin, HIGH);  // clock reset is active LOW
-  digitalWrite( clockPin, LOW);    // shift occurs on rising edge of clock
-  
-}
 
 void shiftOutByte( byte b)
 {
@@ -84,14 +75,61 @@ void convertLongInt( long int num, byte segData[])
     segData[i] = byteToSegDigit( digit);
   }
 }
+
+void convertFloat( float num, byte segData[])
+{
+  char cbuf[16];
+  for( byte i = 0; i < 16; i += 1)
+    cbuf[i] = ' ';
+  
+  // dtostrf(floatVar, minStringWidthIncDecimalPoint, numVarsAfterDecimal, charBuf)
+  dtostrf(num, 8, 3, cbuf);
+  
+  byte i = 0;
+  byte j = 0;
+  while( i < 8)
+  {
+    if(cbuf[j] == '.')
+    {
+      segData[7-i+1] |= 0x01;  // set the decimal point (LSB) of the previous digit
+      j += 1;  // move to the next ascii character
+    } else if( cbuf[j] == ' ')
+    {
+      segData[7-i] = 0x00;
+      i += 1; 
+      j += 1;
+    } else if( cbuf[j] == '-')
+    {
+      segData[7-i] = 0b00000010;
+      i += 1; 
+      j += 1;
+    } else if( cbuf[j] == '\0')
+    {
+      i = 8;  // stop this crazy train cause we've run out of things to parse! 
+    }
+    else
+    {
+      segData[7-i] &= 0x01;  // blank out the segments, but save the decimal point
+      segData[7-i] |= byteToSegDigit( cbuf[j] - '0' );
+      i += 1;  // move to next display digit
+      j += 1;  // move to next ascii character
+    }
+  }
+}
+
+void blankDisplay( byte segData[])
+{
+  for( byte i = 0; i < 8; i += 1)
+    segData[i] = 0x00;
+}
   
 long int count = 1;
 long int lastTime = 0;
 byte displayData[8] = { 0xFF, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
 void loop()
-{
-  
-  convertLongInt( count, displayData);
+{  
+  //convertLongInt( count, displayData);
+  convertFloat( sin(count/360.0), displayData);
   displayAllSegments(displayData);  // call this as fast as possible to avoid obvious scanning
   
   if( millis() > lastTime + 10)
@@ -99,4 +137,16 @@ void loop()
     count += 1;
     lastTime = millis();
   }
+}
+
+void setup()
+{
+  pinMode( dataPin, OUTPUT);
+  pinMode( clockPin, OUTPUT);
+  pinMode( rclockPin, OUTPUT);
+
+  digitalWrite( rclockPin, HIGH);  // clock reset is active LOW
+  digitalWrite( clockPin, LOW);    // shift occurs on rising edge of clock
+ 
+  blankDisplay( displayData); 
 }
